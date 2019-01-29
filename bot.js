505ea20ceae299321ba0ac4fbf29cebcccdfd522
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-01-29 22:35:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = "?+";
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
